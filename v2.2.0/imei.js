66ae9f6c6d16eef1446de2f8254e69c22b02e614
Simon MacDonald <simon.macdonald@gmail.com>
2013-02-01 19:41:59 -0500

    Whoops forgot to copy over the imei.js file

--- v2.2.0/imei.js
+++ v2.2.0/imei.js
@@ -1,12 +1,20 @@
-var IMEI = function(){};
+cordova.define("cordova/plugin/imei",
+  function(require, exports, module) {
+    var exec = require("cordova/exec");
 
-IMEI.prototype.get = function(onSuccess, onFail){
-    return cordova.exec(onSuccess, onFail, 'IMEI', 'get', []);
-};
+    var IMEI = function(){};
+    
+    IMEI.prototype.get = function(onSuccess, onFail){
+        return cordova.exec(onSuccess, onFail, 'IMEI', 'get', []);
+    };
+
+    var imei = new IMEI();
+    module.exports = imei;
+});
 
 if(!window.plugins) {
     window.plugins = {};
 }
 if (!window.plugins.imei) {
-    window.plugins.imei = new IMEI();
+    window.plugins.imei = cordova.require("cordova/plugin/imei");
 }
